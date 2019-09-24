18998fb6869f8b991ac4e4963904c7efbe76ecd2
THOMAIN-PIERRE <pierrethomain@yahoo.fr>
2019-09-24 10:21:08 +0200

    Modif LivreOr.html, mentions_legales.html, politique_de_confidentialité renommé,restaurant.html modifié, création style.css, création template.inc.php

--- Accueil.php
+++ Accueil.php
@@ -21,23 +21,23 @@
         }
         /*Choix de la langue pour le site*/
         p.langue{
-            position: absolute;
+            position:absolute;
             top: 15px;
             left: 5px;
             font-size: 20px;
         }
+        
         p.contact{
-            position:absolute; 
-            top: -62px;
+            position: absolute; 
+            top: -25px;
             right: 5px;
             font-size: 20px;
             right: 
         }
-        }
-        /*Nom du restaurant*/
+        /*Nomdu restaurant*/
         #mansalva{
-            position: relative;
-            bottom: 250px;
+            position: absolute;
+            top: 250px;
             left: 500px;
             font-family: 'Mansalva', cursive;
             font-size: 80px;
@@ -51,6 +51,7 @@
         #image_entete{
             width: 1517px;
             height: 400px;
+
         }
         body{
             background-color:#7c7878;
@@ -67,7 +68,7 @@
             font-family: 'Lato', sans-serif;
             
         }
-        body, h1, p{
+        body, h1, h2, p{
             margin: 0;
 
         }
@@ -85,7 +86,7 @@
             padding: 5px;
             background-color: #C0C0C0;
         }
-        #haut h1{
+        #haut h1, #haut h2 {
             color:white;
         }
         /*Contenu principal*/
@@ -98,7 +99,7 @@
 
         }
         #haut{
-            font-size: 25px;
+            font-size: 30px;
             font-weight: bold;
             border-radius: 40px;
             text-align: center;
@@ -116,31 +117,24 @@
         }
 
         article{
-            margin: 0 0 40px 0;
+            margin: 0 0 20px 0;
             border-top: 2px dotted white;
-            text-align: justify;
 
         }
         #navigation{
-            
-            background-color:#7c7878;
-            margin-top: 30px;
-            margin-bottom: 30px;
-            margin-left: 0px;
+            margin: 0 0 5px 0;
+            padding: 0;
+            background-color: blue;
+            margin-top: 20px;
+            margin-bottom: 20px;
+
         }
         #navigation a{
             text-decoration: none;
             color: white;
-            font-size: 31px;
-            font-weight: bold;
-            font-family: 'Mansalva', cursive;
-            padding: 20px 30px;
-            
+            font-size: 17px;
+            margin-bottom: 15px;
         }
-        #navigation a:hover {
-         background-color: blue;
-        }
-
         #chef{
            float:left;
            margin-top: 5px;
@@ -148,16 +142,19 @@
           
         }
         .listemenus{
-            font-size: 30px;
+            font-size: 35px;
             font-weight: bold;
             list-style: none;
             text-align: center;
             margin-top: 50px; 
             color: white;  
         }
-       
+        #menus{
+            font-weight: bold;
+            margin-top: 20px;
+            
+        }
         .menu_type{
-            color:orange; 
             font-size: 80px;
             font-weight: bold;
             text-align: center;
@@ -212,18 +209,15 @@
         
         .contact{
             text-align: center;
-            margin-top: 80px;
+            margin-top: 40px;
             margin-left: 28px;
             font-size: 30px;
             font-weight: bold;
-            color: white;
-            
+            color: white
         }
-            
         .horaires{
             text-align: center;
-            margin-top: 60px;
-            margin-bottom: 20px;
+            margin-top: 100px;
             font-size: 30px;
             font-weight: bold;
             color: white
@@ -244,7 +238,7 @@
         font-weight: bold;
         color: white;
         text-align: center;
-        padding: 20px 46.2px;
+        padding: 20px 45.8px;
         text-decoration: none;
         }
 
@@ -307,10 +301,11 @@
         #footer{
             background-color: black;
             clear: both;
-            height: 220px;
+            height: 210px;
             }
 
         #footer a{
+        
             font-size: 20px;
             font-weight: bold;
             color: white;
@@ -318,23 +313,26 @@
             margin-left: 110px;
         
         }
-        #infos_complémentaires{
+        #facebook{
             position: relative;
-            left: 160px;
-            top: 20px;
-            
+            left: 400px;
+            bottom: 10px;
         }
 
-        #logos{
+        #instagram{
             position: relative;
-            left: 330px;
-            top: 50px;
+            left: 200px;
+            bottom: 10px;
         }
-        
+        #twitter{
+            position: relative;
+            right: 10px;
+            bottom: 18px;
+        }
         #haut_de_page{
             position: relative;
+            right: 550px;
             top: 80px;
-            left: 570px;
             
         }
        
@@ -347,49 +345,55 @@
         </div>   
     </header>
     <nav class="navbar">
-            <a href="Accueil.html" target="_blank">ACCUEIL</a>
+            <a href="test.html" target="_blank">ACCUEIL</a>
             <a href="Restaurant.html" target="_blank">RESTAURANT</a>
             <a href="Carte.html" target="_blank">CARTE</a>
                 <div class="dropdown">
-                    <button class="dropdown-button">RECETTES
-                    <i class="fa fa-caret-down"></i>
-                    </button>
-                <div class="dropdown-content">
-                    <a href="recette1.html">Asperges rôties et oeuf poché au magret fumé</a>
-                    <a href="recette2.html">Foie gras de canard chaud aux clémentines</a>
-                    <a href="recette3.html">Poulet basquaise bleu-blanc coeur</a>
-                    <a href="recette4.html">Gratin de légume aux piments d'espelette</a>
-                    <a href="recette5.html">Poulet basquaise</a>
-                </div>
-                </div>
+                        <button class="dropdown-button">RECETTES
+                          <i class="fa fa-caret-down"></i>
+                        </button>
+                        <div class="dropdown-content">
+                          <a href="recette1.html">Asperges rôties et oeuf poché au magret fumé</a>
+                          <a href="recette2.html">Foie gras de canard chaud aux clémentines</a>
+                          <a href="recette3.html">Poulet basquaise bleu-blanc coeur</a>
+                          <a href="recette4.html">Gratin de légume aux piments d'espelette</a>
+                          <a href="recette5.html">Poulet basquaise</a>
+                        </div>
+                      </div>
+                    </div>
             <a href="LivreOr.html" target="_blank">LIVRE D'OR</a>
-            <a href="Reservation.html" target="_blank">RESERVATION</a>
-            </nav>
-            <img id="image_entete" src="https://mk0hotellenouveowyay.kinstacdn.com/wp-content/uploads/2019/01/restaurant-7-mers-bandeau.jpg" alt="photo restaurant"/>
+            <a href="Réservation.html" target="_blank">RESERVATION</a>
             
-           
+            <img id="image_entete" src="https://mk0hotellenouveowyay.kinstacdn.com/wp-content/uploads/2019/01/restaurant-7-mers-bandeau.jpg" alt="photo restaurant"/>
+            <p id="mansalva">LE COUNIC</p>
+            
             <div id="conteneur">
-        <div id="haut">
+        <header id="haut">
                 <h1>PLUS QU'UN RESTAURANT ... UNE EXPERIENCE GUSTATIVE</h1>     
-        </div>
+        </header>
             <section id="contenu">
-                <article>
+                <article class="article_un">
                     <h1>UN RESTAURANT MILITANT</h1>
                     <p>Le Counic est un concept de <strong>restaurant écoresponsable</strong> dont le but est d'offrir à tous l'accès à des <strong>produits frais, fabriqués localements par de petits producteurs</strong>. Un plât spécial est ainsi crée chaque jour à partir de produits 100% régionaux par notre chef. Dès la conception de nos menus, nous veillons à limiter notre empreinte carbone en choisissant des produits de saison issus de <strong>l’agriculture biologique locale</strong>, en veillant à l’équilibre légumes / viande du plat. Nous <strong>recyclons nos épluchures et réduisons les portions</strong> pour éviter le gaspillage alimentaire. Nous veillons également à <strong>limiter notre consommation d'eau et d'énergie</strong>. C'est pourquoi, l'eau de nettoyage des légumes est réutilisée pour arroser les herbes aromatiques qui servent à l'élaboration de vos plâts. En salle, la décoration et le mobilier reflétent notre engagement. Nous avons choisis des <strong>matériaux nobles et durables</strong> comme le bois et un éclairage à LED pour réduire la consommation d’énergie. Voilà les valeurs que nous défendons et les engagements que nous prenons.</p> 
                     <p>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker.</p>
                 </article>
-                <article>
+                <article class="article_deux">
                     <h1>UN CHEF D'EXCEPTION</h1>
+                    <div>
                     <img id="chef" src="https://previews.123rf.com/images/rh2010/rh20101702/rh2010170200194/71846768-chef-cuisinier-%C3%A0-la-cuisine.jpg" alt="Mister Cuisto" width="330px" height="250px" border="2px">
                     <p>Formé à l'école Joel Robuchon, notre chef <strong>Jean Bertomieu (ou Mister Cool pour ses équipes)</strong> a seulement 29 ans, quand il prend les commandes des cuisines de <trong>l'hôtel Concorde Lafayette</strong> et dirige 90 cuisiniers, servant plusieurs milliers de repas par jour. Son professionnalisme, sa rigueur, son assurance, sa créativité lui taillent déjà une belle réputation. Après un passage remarqué en tant que chef et directeur de la restauration de <strong>l'hôtel Nikko</strong> où il décochera deux étoiles, <strong>il ouvre son restaurant à Oloron Sainte Marie en décembre 2010 : Le Counic</strong>.</p>
+                    </div>
                 </article>
-                <article>
-                        <h1>CARTE / MENUS</h1>
+                <br>
+                <br>
+                <article class="article_trois">
+                        <h1>NOTRE CARTE</h1>
                         <p>Tous les jours, en plus de vos plâts habituels, retrouvez le plât de jour du chef garanti 100% régional et 100% écoresponsable. <strong>Cliquez sur les liens ci-dessous pour découvrir les plâts spécialement conçus pour vous cette semaine :</strong></p>
                     <nav id="navigation">   
-                        <a href="Carte.html#jour1">Lundi</a><a href="Carte.html#jour2">Mardi</a><a href="Carte.html#jour3">Mercredi</a><a href="Carte.html#jour4">Jeudi</a><a href="Carte.html#jour5">Vendredi</a><a href="Carte.html#jour6">Samedi</a>
+                        <p><a href="Carte.html">Lundi</a><a href="Carte.html">Mardi</a><a href="Carte.html">Mercredi</a><a href="Carte.html">Jeudi</a><a href="Carte.html">Vendredi</a><a href="Carte.html">Samedi</a></p>
                     </nav>
                         <p>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker.</p> 
+                        <p id="menus">RETROUVEZ NOS MENUS CI-DESSOUS :</p>
                         <p class="menu_type">DEJEUNER (Entrée-Plât-Dessert, hors boissons)</p>
                         <ul class="listemenus">
                             <li>Menu Eco-responsable &rarr;  20 euros</li>
@@ -403,7 +407,6 @@
                             <li>Menu Prestige        &rarr;  40 euros</li>   
                         </ul>
                 </article>
-                <p id="mansalva">LE COUNIC</p>
             </section>
             <aside id="sidebar">
                 <div>
@@ -429,22 +432,17 @@
                     <p>Du Lundi au Samedi</p>
                     <p>Midi : 11h à 15h</p>
                     <p>Soir : 19h à 23h</p>   
-                </div>
+                </div>  
             </aside>
         <footer id="footer">
-                <div id="infos_complémentaires">    
-                    <a href="Mentions_Legales.html" target="_blank">Mentions légales</a></li>
-                    <a href="Politique_de_confidentialite.html" target="_blank">Politique de confidentialité</a></li>
-                    <a href="https://www.pyrenees-bearnaises.com/pratique/presentation-de-loffice-de-tourisme-du-haut-bearn/" target="_blank">Office du tourisme</a></li>
-                </div> 
-                <div id="logos"> 
+                    <a href="Mentions Légales.html" target="_blank">Mentions légales</a></li>
+                    <a href="Politique de confidentialité.html" target="_blank">Politique de confidentialité</a></li>
+                    <a href="https://www.pyrenees-bearnaises.com/pratique/presentation-de-loffice-de-tourisme-du-haut-bearn/" target="_blank">Office de tourisme</a></li>
                     <a id="facebook" href="https://facebook.com" target="_blank"><img src="https://cdn.pixabay.com/photo/2015/05/17/10/51/facebook-770688_960_720.png" alt="logo Facebook" width="60px" height="60px"/></a>
                     <a id="instagram" href="https://instagram.com" target="_blank"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/220px-Instagram_logo_2016.svg.png" alt="logo Instagram" width="60px" height="60px"/></a>
                     <a id="twitter" href="https://twitter.com" target="_blank"><img src="https://ciervoblanco.club/wp-content/uploads/2016/04/ciervo-blanco-club-de-lectura-twitter-logo.png" alt="logo Twitter" width="75px" height="75px"/></a>
-                 </div>   
                     <a id="haut_de_page" href="#Top">Haut de page</a>
-        </footer>
-    </div>
+        <footer>
     </body>
 </html>
 
---
+++ template.inc.php
@@ -0,0 +1,62 @@
+
+    <header> 
+        <div class="up">
+        <p class="langue">Français | English</p>
+        <p class="contact"> Contact &nbsp;&nbsp;05 59 39 70 23</p>
+        </div>   
+    </header>
+    <nav class="navbar">
+            <a href="test.html" target="_blank">ACCUEIL</a>
+            <a href="Restaurant.html" target="_blank">RESTAURANT</a>
+            <a href="Carte.html" target="_blank">CARTE</a>
+                <div class="dropdown">
+                        <button class="dropdown-button">RECETTES
+                          <i class="fa fa-caret-down"></i>
+                        </button>
+                        <div class="dropdown-content">
+                          <a href="recette1.html">Asperges rôties et oeuf poché au magret fumé</a>
+                          <a href="recette2.html">Foie gras de canard chaud aux clémentines</a>
+                          <a href="recette3.html">Poulet basquaise bleu-blanc coeur</a>
+                          <a href="recette4.html">Gratin de légume aux piments d'espelette</a>
+                          <a href="recette5.html">Poulet basquaise</a>
+                        </div>
+                      </div>
+                    </div>
+            <a href="LivreOr.html" target="_blank">LIVRE D'OR</a>
+            <a href="Réservation.html" target="_blank">RESERVATION</a>
+            
+            <img id="image_entete" src="https://mk0hotellenouveowyay.kinstacdn.com/wp-content/uploads/2019/01/restaurant-7-mers-bandeau.jpg" alt="photo restaurant"/>
+            <p id="mansalva">LE COUNIC</p>
+            <div id="conteneur">
+        <footer id="footer">
+                    <a href="Mentions Légales.html" target="_blank">Mentions légales</a></li>
+                    <a href="Politique de confidentialité.html" target="_blank">Politique de confidentialité</a></li>
+                    <a href="https://www.pyrenees-bearnaises.com/pratique/presentation-de-loffice-de-tourisme-du-haut-bearn/" target="_blank">Office de tourisme</a></li>
+                    <a id="facebook" href="https://facebook.com" target="_blank"><img src="https://cdn.pixabay.com/photo/2015/05/17/10/51/facebook-770688_960_720.png" alt="logo Facebook" width="60px" height="60px"/></a>
+                    <a id="instagram" href="https://instagram.com" target="_blank"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/220px-Instagram_logo_2016.svg.png" alt="logo Instagram" width="60px" height="60px"/></a>
+                    <a id="twitter" href="https://twitter.com" target="_blank"><img src="https://ciervoblanco.club/wp-content/uploads/2016/04/ciervo-blanco-club-de-lectura-twitter-logo.png" alt="logo Twitter" width="75px" height="75px"/></a>
+                    <a id="haut_de_page" href="#Top">Haut de page</a>
+        </footer>
+    </body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
